42e5c34982dc35df239270bde5245af4cdba4c65
DaisyCoding <qq1396037294@hotmail.com>
2025-11-10 19:51:23 +0800

    完成界面ui设计

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    statusLabel.setMinimumWidth(120);
+    statusLabel.setText(("length:  "+QString::number(0)+"  lines:  "+QString::number(1)));
+    ui->statusbar->addPermanentWidget(&statusLabel);
+
+    statusCursorLabel.setMinimumWidth(90);
+    statusCursorLabel.setText(("Ln:  "+QString::number(0)+"  Col:  "+QString::number(1)));
+    ui->statusbar->addPermanentWidget(&statusCursorLabel);
+
+    QLabel *author = new QLabel(ui->statusbar);
+    author->setText(tr("章皖俊"));
+    ui->statusbar->addPermanentWidget(author);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
